c502355bcdefeac707481eb44cfe8513daf2d4e9
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-12-29 08:59:38 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# gphoto_2_album
+
+Return photo list and caption (markdown format) from weibo.
+
+## usage
+
+```
+import gphoto_2_album
+result = gphoto_2_album.get(url)
+result.imgs
+result.cap
+```
+
+## how to install
+
+`pip3 install gphoto_2_album`
---
+++ test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import gphoto_2_album
+import yaml
+from telegram.ext import Updater
+import album_sender
+import os
+
+with open('CREDENTIALS') as f:
+	CREDENTIALS = yaml.load(f, Loader=yaml.FullLoader)
+tele = Updater(CREDENTIALS['bot_token'], use_context=True)
+# chat = tele.bot.get_chat(-1001198682178)
+chat = tele.bot.get_chat('@web_record')
+
+def test(url, rotate=False):
+	r = gphoto_2_album.get(url)
+	print(r.imgs)
+	print(r.video)
+	# album_sender.send(chat, url, r, rotate = rotate)
+	
+if __name__=='__main__':
+	test('https://m.weibo.cn/detail/4585474025325107#comment')
