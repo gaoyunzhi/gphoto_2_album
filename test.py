a76a20ce66c5787c58c3b123278ac3f7d45ab4b2
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-12-29 09:03:07 -0800

    test

--- test.py
+++ test.py
@@ -7,17 +7,16 @@ from telegram.ext import Updater
 import album_sender
 import os
 
-with open('CREDENTIALS') as f:
+with open('credentials') as f:
 	CREDENTIALS = yaml.load(f, Loader=yaml.FullLoader)
 tele = Updater(CREDENTIALS['bot_token'], use_context=True)
-# chat = tele.bot.get_chat(-1001198682178)
-chat = tele.bot.get_chat('@web_record')
+chat = tele.bot.get_chat(-1001198682178)
+# chat = tele.bot.get_chat('@web_record')
 
 def test(url, rotate=False):
 	r = gphoto_2_album.get(url)
 	print(r.imgs)
-	print(r.video)
-	# album_sender.send(chat, url, r, rotate = rotate)
+	album_sender.send(chat, url, r, rotate = rotate)
 	
 if __name__=='__main__':
-	test('https://m.weibo.cn/detail/4585474025325107#comment')
+	test('https://photos.google.com/share/AF1QipOtOHHWLLtS9RV6yMuIpT9qpUSnoAv2tRg9OpEH27wkf39qU-cRTpn9uJGOd_FTpw?key=a0k1NHNoYmdvY256Y2oyVWd3aXV1TlNJOUk4Wjl3')
